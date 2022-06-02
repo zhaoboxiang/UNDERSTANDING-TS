2fc4367444fc297487e78c3bc37c83c3ede0d155
zhaoboxiang <zbx@hcttop.com>
2022-06-02 16:55:51 +0800

    refactor: ts course was end

--- app.ts
+++ app.ts
@@ -1,18 +1,1 @@
-function addCb(n1: number, n2: number) {
-  return n1 + n2;
-}
-
-function printResultFn(result: number): void {
-  console.log("Result is: " + result);
-}
-
-console.log(printResultFn(addCb(33, 22)));
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(12, 44, (result) => {
-  console.log(result);
-});
+console.log("typescript config....");
