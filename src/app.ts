979eaadb91ad950ed7ccdbb6b3f8be0e95bf2199
zhaoboxiang <zbx@hcttop.com>
2022-06-14 10:26:02 +0800

    refactor: es6

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("typescript config....22");
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
+
+printOutput(add(5, 2));
+
+const sum = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const sumNumbers = sum(5, 10, 22, 3.9);
+
+console.log(sumNumbers);
